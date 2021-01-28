77ea79ab2ee5d437073c9eccaeb091575cc80092
MrBlubke <ward@impesoft.com>
2021-01-28 22:00:43 +0100

    dynamically added to dynamically created stackpanel

--- Big Bucks Coffee/MainWindow.xaml.cs
+++ Big Bucks Coffee/MainWindow.xaml.cs
@@ -23,6 +23,18 @@ namespace Big_Bucks_Coffee
         public MainWindow()
         {
             InitializeComponent();
+            int aantalkeer = 3;
+            Content = ShowMyControl(aantalkeer);
+        }
+
+        private StackPanel ShowMyControl(int aantalkeer)
+        {
+            var stackPanel = new StackPanel { Orientation = Orientation.Horizontal };
+            for (int i = 0; i < aantalkeer; i++)
+            {
+                stackPanel.Children.Add(new UserControl1());
+            }
+            return stackPanel;
         }
     }
 }
