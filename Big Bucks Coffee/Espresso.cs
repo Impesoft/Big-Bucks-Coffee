5cfd849041804e019ae3cdd4c590915e4bdfcf5a
MrBlubke <ward@impesoft.com>
2021-01-28 21:01:02 +0100

    Add project files.

---
+++ Big Bucks Coffee/Espresso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Big_Bucks_Coffee
+{
+    internal class Espresso : Coffee
+    {
+        public string Roast { get; set; }
+
+        public Espresso()
+        {
+        }
+    }
+}
---
+++ Big Bucks Coffee/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Big_Bucks_Coffee
+{
+    internal class ShoppingCart
+    {
+        private List<IBeverage> _beverages;
+
+        public void AddItemToCart(IBeverage beverage)
+        {
+            _beverages.Add(beverage);
+        }
+
+        public double CalculatePrice(IEnumerable<IBeverage> beverages)
+        {
+            return 0.0;
+        }
+
+        public double CalculateTax()
+        {
+            return 0.0;
+        }
+
+        public void ClearCart()
+        {
+            _beverages.Clear();
+        }
+
+        public List<IBeverage> GetItemsInCart()
+        {
+            return new List<IBeverage>();
+        }
+    }
+}
