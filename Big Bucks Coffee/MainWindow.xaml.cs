2604bd071cc3f0d53a145a54f1ac371adfe08a91
MrBlubke <ward@impesoft.com>
2021-01-29 13:10:52 +0100

    button bubbling up from usercontrol

--- Big Bucks Coffee/MainWindow.xaml.cs
+++ Big Bucks Coffee/MainWindow.xaml.cs
@@ -23,18 +23,59 @@ namespace Big_Bucks_Coffee
         public MainWindow()
         {
             InitializeComponent();
-            int aantalkeer = 3;
-            Content = ShowMyControl(aantalkeer);
+            int aantalkeer = 5;
+            string[] name = new string[5] { "Appels", "Peren", "Bananen", "Appelsienen", "Citroenen" };
+            //   Content = ShowMyControl(aantalkeer);
+            ShowMyControl(aantalkeer, name);
+            foreach (var control in this.myWrap.Children)
+            {
+                UserControl1 myUserControl = control as UserControl1;
+
+                if (myUserControl != null)
+                {
+                    myUserControl.AddToCartButtonClicked += AddToCartButtonClickedInUserControl;
+                }
+            }
+            //  this.Width = this.Width - (this.Width % 250);
         }
 
-        private StackPanel ShowMyControl(int aantalkeer)
+        //        private Grid ShowMyControl(int aantalkeer)
+        private void ShowMyControl(int aantalkeer, string[] name)
         {
-            var stackPanel = new StackPanel { Orientation = Orientation.Horizontal };
             for (int i = 0; i < aantalkeer; i++)
             {
-                stackPanel.Children.Add(new UserControl1());
+                var g = new UserControl1
+                {
+                    Name = name[i],
+                    HorizontalAlignment = HorizontalAlignment.Left,
+                    Height = 420
+                };
+                g.ProductName.Content = name[i];
+                g.Price.Content = $"Price: € {i * 5 + 1}";
+                g.TextBox.Text = $"Hello {i} World";
+                g.IsInStock.IsChecked = (i % 2 == 0);
+                myWrap.Children.Add(g);
             }
-            return stackPanel;
+        }
+
+        private void AddToCartButtonClickedInUserControl(object sender, EventArgs e)
+        {
+            // Cast object class to MyUserClass
+            UserControl1 myControl = sender as UserControl1;
+            System.Windows.MessageBox.Show(myControl.ProductName.Content.ToString());
+            //     int productId = myControl.PizzaID;
+            //   AddProductToCart(productId);
+        }
+
+        private void AddProductToCart(int productId)
+        {
+            //   var pizza = _repo.GetPizza(productId);
+            //    _cart.AddProductToCart(pizza);
+        }
+
+        private void MainGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //this.Width = this.Width - (this.Width % 250);
         }
     }
 }
--- Big Bucks Coffee/UserControl1.xaml.cs
+++ Big Bucks Coffee/UserControl1.xaml.cs
@@ -22,5 +22,20 @@ namespace Big_Bucks_Coffee
         {
             InitializeComponent();
         }
+
+        // Event Handler -> Raise an event from a child class
+        // Delegate field -> Accepts a method instead of a value or instance
+        public event EventHandler AddToCartButtonClicked;
+
+        protected virtual void OnAddToCartButtonClicked(EventArgs e)
+        {
+            AddToCartButtonClicked?.Invoke(this, e);
+        }
+
+        private void AddToCart_Click(object sender, RoutedEventArgs e)
+        {
+            // Pass child event to parent by raising it in an event which can be caught in parent
+            OnAddToCartButtonClicked(e);
+        }
     }
 }
